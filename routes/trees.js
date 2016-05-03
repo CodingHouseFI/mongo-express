cca9beeb7260f9ae96582260ad8fe522e2130be2
Cade Nichols <cade@cadenichols.com>
2016-05-03 10:18:43 -0700

    ready for heroku

--- routes/trees.js
+++ routes/trees.js
@@ -3,69 +3,35 @@ var router = express.Router();
 
 var Tree = require('../models/tree');
 
-//  POST  /api/trees   --->  create a new tree
- 
-//  GET   /api/trees   --->  return array of all trees
-router.get('/', (req, res) => {
-  Tree.find({}, (err, trees) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(trees);
-    }
-  });
-});
 
-//  GET   /api/trees/:id   --->  return one tree by id
-router.get('/:id', (req, res) => {
+router.route('/')
+  .get((req, res) => {
+    Tree.find({}, (err, trees) => {
+      res.status(err ? 400 : 200).send(err || trees);
+    });
+  })
+  .post((req, res) => {
+    var tree = new Tree(req.body);
+    tree.save((err, savedTree) => {
+      res.status(err ? 400 : 200).send(err || savedTree);
+    });
+  })
 
-  Tree.findById(req.params.id, (err, tree) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(tree);
-    }
-  });
-
-});
-
-//  DELETE   /api/trees/:id   --->  delete one tree by id
-router.delete('/:id', (req, res) => {
-  Tree.findByIdAndRemove(req.params.id, err => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send();
-    }
-  });
-});
-
-//  POST  /api/trees   --->  create a new tree
-router.post('/', (req, res) => {
-  var tree = new Tree(req.body);
-
-  tree.save((err, savedTree) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(savedTree);
-    }
-  });
-});
-
-//  PUT   /api/trees/:id   --->  update one tree
-router.put('/:id', (req, res) => {
-
-  // req.params.id  -->  document id
-  // req.body  --->  update obj
-
-  Tree.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }, (err, tree) => {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      res.send(tree);
-    }
-  });
-});
+router.route('/:id')
+  .get((req, res) => {
+    Tree.findById(req.params.id, (err, tree) => {
+      res.status(err ? 400 : 200).send(err || tree);
+    });
+  })
+  .delete((req, res) => {
+    Tree.findByIdAndRemove(req.params.id, (err, tree) => {
+      res.status(err ? 400 : 200).send(err);
+    });
+  })
+  .put((req, res) => {
+    Tree.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }, (err, tree) => {
+      res.status(err ? 400 : 200).send(err || tree);
+    });
+  })
 
 module.exports = router;
